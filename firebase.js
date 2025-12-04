cfb7931a55b147de1898a7171bc9df47054d68db
Green <idk1234512345121@gmail.com>
2025-12-04 00:18:55 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,31 @@
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCKz-sX0ZSzwJmdGr0kqcUFCHAehgZqT6o",
-  authDomain: "greenlet-7919f.firebaseapp.com",
-  projectId: "greenlet-7919f",
-  storageBucket: "greenlet-7919f.appspot.com",
-  messagingSenderId: "68860296566",
-  appId: "1:68860296566:web:2da85f8f286ca7ca434e57",
-  measurementId: "G-0D3KD3NZMK"
-};
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKz-sX0ZSzwJmdGr0kqcUFCHAehgZqT6o",
+    authDomain: "greenlet-7919f.firebaseapp.com",
+    projectId: "greenlet-7919f",
+    storageBucket: "greenlet-7919f.firebasestorage.app",
+    messagingSenderId: "68860296566",
+    appId: "1:68860296566:web:2da85f8f286ca7ca434e57",
+    measurementId: "G-0D3KD3NZMK"
+  };
 
-// Firebase services
-const auth = firebase.auth();
-const db = firebase.firestore();
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  document.getElementById("loginBtn").addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const pass  = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, pass)
+      .then((userCredential) => {
+        console.log("Logged in!");
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  });
+</script>
