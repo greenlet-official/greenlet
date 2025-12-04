ddedac252fe8bedea90a74c03b14414192b2879a
Green <idk1234512345121@gmail.com>
2025-12-04 00:09:43 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // Replace the firebaseConfig with your project's config from Firebase console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_APP.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_APP.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCKz-sX0ZSzwJmdGr0kqcUFCHAehgZqT6o",
+  authDomain: "greenlet-7919f.firebaseapp.com",
+  projectId: "greenlet-7919f",
+  storageBucket: "greenlet-7919f.firebasestorage.app",
+  messagingSenderId: "68860296566",
+  appId: "1:68860296566:web:2da85f8f286ca7ca434e57"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
